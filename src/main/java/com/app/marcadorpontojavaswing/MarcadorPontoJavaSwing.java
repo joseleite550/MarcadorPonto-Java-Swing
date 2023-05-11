996e5e58d339287949726a3a317d189ff3c35884
Jose Leite <122051639+joseleite550@users.noreply.github.com>
2023-05-11 11:37:25 -0300

    commit inicial

---
+++ src/main/java/com/app/marcadorpontojavaswing/MarcadorPontoJavaSwing.java
@@ -0,0 +1,8 @@
+package com.app.marcadorpontojavaswing;
+
+public class MarcadorPontoJavaSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
